dffac522a41c431b82bbed58d1ff5186b3154659
Ryan Arnold <arnold.1016@osu.edu>
2019-08-20 17:20:07 -0400

    started some code

--- scan_excel.py
+++ scan_excel.py
@@ -1,7 +1,30 @@
 import pandas as pd 
 import numpy as np 
 import matplotlib.pyplot as plt  
+import pickle
+
+def storeData(obj,str):
+    file = open(str,'ab')
+    pickle.dump(obj,file)
+    file.close()
+    
+def loadData(fileName):
+    file = open(fileName,'rb')
+    obj = pickle.load(file)
+    file.close()
+    return obj 
 
 fileName = 'Polymers_Photobleaching_Master copy 2.xlsx'
 
-xlDataFrame = pd.read_excel(fileName,)
+xl = pd.ExcelFile(fileName)
+
+sheetNames = xl.sheet_names
+
+badNames = ['Analysis','Analysis 2']
+
+for sht in sheetNames:
+    if sht not in badNames:  
+        df = pd.read_excel(xl,sheet_name = sht)
+
+
+
